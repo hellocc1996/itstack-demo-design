305b0b3f120ee7eff650022d96ba2dab8a836d54
cuipingping <cuipingping@nicomama.com>
2020-11-26 14:29:41 +0800

    优化

--- itstack-demo-design-1-02/src/main/java/org/itstack/demo/design/StoreFactory.java
+++ itstack-demo-design-1-02/src/main/java/org/itstack/demo/design/StoreFactory.java
@@ -7,11 +7,25 @@ import org.itstack.demo.design.store.impl.GoodsCommodityService;
 
 public class StoreFactory {
 
+    /**
+     * 工厂类
+     * <p>
+     * 如果你不喜欢if判断，也可以使用switch或者map配置结构，会让代码更加干净
+     * 也可以使用反射或者结合其他设计模式组合使用
+     *
+     * @param commodityType
+     * @return
+     */
     public ICommodity getCommodityService(Integer commodityType) {
-        if (null == commodityType) return null;
-        if (1 == commodityType) return new CouponCommodityService();
-        if (2 == commodityType) return new GoodsCommodityService();
-        if (3 == commodityType) return new CardCommodityService();
+        if (null == commodityType) {
+            return null;
+        } else if (1 == commodityType) {
+            return new CouponCommodityService();
+        } else if (2 == commodityType) {
+            return new GoodsCommodityService();
+        } else if (3 == commodityType) {
+            return new CardCommodityService();
+        }
         throw new RuntimeException("不存在的商品服务类型");
     }
 
--- itstack-demo-design-1-02/src/main/java/org/itstack/demo/design/store/ICommodity.java
+++ itstack-demo-design-1-02/src/main/java/org/itstack/demo/design/store/ICommodity.java
@@ -4,6 +4,17 @@ import java.util.Map;
 
 public interface ICommodity {
 
+    /**
+     * 所有的奖品无论是实物、虚拟还是第三方，都需要通过我们的程序实现此接口进行处理，以保证最终入参出参的统一性。
+     * <p>
+     * 接口的入参包括；用户ID、奖品ID、业务ID以及扩展字段用于处理发放实物商品时的收获地址。
+     *
+     * @param uId
+     * @param commodityId
+     * @param bizId¬
+     * @param extMap
+     * @throws Exception
+     */
     void sendCommodity(String uId, String commodityId, String bizId, Map<String, String> extMap) throws Exception;
 
 }
